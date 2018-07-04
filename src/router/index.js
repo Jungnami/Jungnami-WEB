7b9cdc67c2e685585aba83d880bcace019b89ad0
RyuDongIl <jules010@naver.com>
2018-07-04 17:14:39 +0900

    지도 임시 완료

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Main'
 import Legislator from '@/pages/legislator/Main'
+import LegislatorList from '@/pages/legislator_list/Main'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/legislator',
       name: 'Legislator',
       component: Legislator
+    },
+    {
+      path: '/list',
+      name: 'LegislatorList',
+      component: LegislatorList
     }
   ]
 })
