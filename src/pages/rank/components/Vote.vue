e8667bda801e452db9947b647ac6b38772eae848
tjdcks12 <tjdcks_12@naver.com>
2018-07-04 19:28:15 +0900

    순위페이지 투표 바 작업중

--- src/pages/rank/components/Vote.vue
+++ src/pages/rank/components/Vote.vue
@@ -1,231 +1,192 @@
 <template>
-<v-container style="height:500px;">
+<div class="vote_container">
+
+  <div class="vote_content">
 
 
-  <v-layout column justify-end>
-    <v-flex xs12 sm6 offset-sm3>
+  <div class="rank" > <!--inline block-->
+    <div class="img">
+    </div>
+    <div class="content">
+          1
+    </div>
+  </div>
 
+  <div class="thumbnail_box"><!--inline block-->
 
-      <ul class="items">
-        <li v-for="(item, index) in items">
-          <input type="checkbox" id="line1" /> {{index}}
-        </li>
-        <li v-for="(item, index) in items">
-          <input type="checkbox" id="line2" /> {{index}}
-        </li>
-        <li v-for="(item, index) in items">
-          <input type="checkbox" id="line3" /> {{index}}
-        </li>
-      </ul>
-    </v-flex>
-  </v-layout>
-</v-container>
+    <div class="thumbnail_border">
+      <img src="../../../../static/img_avatar.png" alt="Avatar" class="thumbnail" >
+    </div>
+
+  </div>
+
+  <div class="progress"><!--inline block-->
+    <div class="bar">
+      <div class="percent">
+
+      </div>
+    </div>
+  </div>
+
+  <div class="emotion"><!--inline block-->
+
+  </div>
+</div>
+</div>
 </template>
 <script>
 export default {
   name: 'Vote',
   data() {
-    return {
-      items: [{
-          header: 'Today'
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/1.jpg',
-          title: 'Brunch this weekend?',
-          subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/2.jpg',
-          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-          subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        },
-        {
-          divider: true,
-          inset: true
-        },
-        {
-          avatar: 'https://s3.amazonaws.com/vuetify-docs/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        }
-      ]
-    }
+    return {}
   }
 }
 </script>
 
 
 <style scoped>
+/*root : 549px*/
+.vote_container {
+  width: 99vw;
+  /*height: auto;*/
+  height: 14vw;
+  position: relative;
+  background-color: rgba(0, 0, 0, 0.05);
+  padding-top: 0.8%;
+  padding-bottom: 1%;
 
-ul.items
+}
+.vote_content {
+  height: 100%;
+  /*padding: 20px;*/
+  display: table;
+}
+
+
+
+.rank {
+  width: 17%;
+  height: 100%;
+  display: table-cell;
+}
+.rank .img
 {
+  width: 50%;
+  margin: 0 auto;
+  height: 90%;
+  background-image: url('../../../../static/gold_medal.png');
+  background-size: contain;
+  align: center;
+  position: relative;
+  top: 5%;
+}
+
+
+.rank .content{
+     position: absolute;
+     top:50%;
+     left: 7.85%;
+     /*left:50%;*/
+     transform: translate(-50%, -50%);
+     font-size:2.7vw;
+     color: white;
+     z-index: 2;
+     text-align: center;
+}
+
+.rank_text{
+  display: inline-block;
+  /*padding: 30px;*/
   text-align: center;
-  list-style-type: none;
-  width: 400px;
 }
 
-ul.items li
+
+
+.thumbnail_box
 {
-    float: left;
-    display: inline;
+  width: 12%;
+  /*height: 60px;*/
+  /*padding-top: 8px;*/
+  /*padding-bottom: 8px;*/
+  /*background-color: #1783DC;*/
+  display: table-cell;
+  /*border-radius: 50%;*/
+  position: relative;
+  /*right: 0px;
+  top: -5px;*/
+  z-index: 1;
+
+
+  /*vertical-align: baseline;*/
 }
 
-.clear { clear: both; }
+.thumbnail_border
+{
+  /*height: */
+}
+
+.thumbnail
+{
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  border : 3px solid #1783DC;
+
+}
+
+
+
+
+.progress
+{
+  width: 300px;
+  height: 100%;
+  position: relative;
+  right: +13px;
+  z-index: 0;
+}
+
+.emotion
+{
+    width:20%;
+    display: table-cell;
+}
+
+
+.bar {
+  /*display: inline-block;*/
+  height: 100%;
+  /*background-color: #1783DC;*/
+  width: 100%;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  /*border-top-right-radius: 25px;
+  border-bottom-right-radius: 25px;*/
+
+}
+
+.percent {
+  height: 100%;
+  background-color: #1783DC;
+  width: 100%;
+  border-top-right-radius: 25px;
+  border-bottom-right-radius: 25px;
+}
+
+
+/*.vote_container {
+  width: 324.29px;
+  height: 60.13px;
+  display: inline-block;
+  background-color: '#000';
+}
+
+.vote_box {
+  width: 300px;
+  background: #000;
+}
+
+.vote_percent {
+  width: 80%;
+}*/
 </style>
