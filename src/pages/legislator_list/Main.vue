b382a04d08ec12e7be6238362c36dc99c2a3127f
RyuDongIl <jules010@naver.com>
2018-07-05 00:57:01 +0900

    메인뷰에 라우터뷰작성

--- src/pages/legislator_list/Main.vue
+++ src/pages/legislator_list/Main.vue
@@ -12,12 +12,7 @@
     </v-flex>
     <v-flex xs1></v-flex>
   </v-layout>
-  <v-layout row wrap>
-    <v-flex xs3 offset-xs1>
-      <img src="../../../static/list_party_text.png" alt="list_party_text" class="party_text">
-      <map-component></map-component>
-    </v-flex>
-  </v-layout>
+  <router-view></router-view>
 </div>
 </template>
 
