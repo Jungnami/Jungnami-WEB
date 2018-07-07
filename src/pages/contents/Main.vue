570a50a13148fef5c6a70d187be2626c2da68dc9
RyuDongIl <jules010@naver.com>
2018-07-07 18:05:07 +0900

    왼쪽 탭바 스크랩 글 작업중

--- src/pages/contents/Main.vue
+++ src/pages/contents/Main.vue
@@ -2,12 +2,20 @@
 <v-layout row nowrap>
   <v-flex sm2 class="hidden-sm-and-down drawer">
     <router-link v-for="(item, i) in link_items" :key=i :to="item.path">
-      <div class="link_box">{{ item.text }}</div>
+      <v-layout row wrap justify-space-between class="link_box">
+        <div>{{ item.text }}</div>
+        <div><img src="/static/contents_rightarrow_btn.png" alt="right_arrow" class="right_icon"></div>
+      </v-layout>
     </router-link>
     <div class="recent_scrap">
       <div class="scrap_title">
         최근 스크랩 글 
-        <img src="../../../static/contents_scrap_icon.png" alt="contents_scrap_icon">
+        <img src="../../../static/contents_scrap_icon.png" alt="contents_scrap_icon" class="scrap_icon">
+      </div>
+      <div v-for="item in recent_items" :key="item.id" class="scrap_card">
+        <img :src="item.image" alt="scrap_card_img" class="card_img">
+        <div class="card_title">{{ item.title }}</div>
+        <div class="card_info">{{ item.category }} · {{ item.recent }}</div>
       </div>
     </div>
   </v-flex>
@@ -27,6 +35,22 @@ export default {
         {text: 'TMI', path: '/contents/TMI'},
         {text: '스토리', path: '/contents/story'}
       ],
+      recent_items: [
+        {
+          id: 17,
+          image: '/static/card_image1.jpeg',
+          title: '문재인 대통령의 살아온 운명',
+          category: '스토리',
+          recent: '11분'
+        },
+        {
+          id: 15,
+          image: '/static/card_image1.jpeg',
+          title: '문재인 대통령의 살아온 일대기와 운명',
+          category: 'TMI',
+          recent: '30분'
+        },
+      ]
     }
   }
 }
@@ -48,11 +72,17 @@ export default {
   margin-left: 1.04vw;
   margin-right: 1.04vw;
   padding-left: 0.52vw;
+  padding-right: 1.56vw;
   padding-top: 1.2vw;
   padding-bottom: 1.2vw;
   border-bottom: 1px solid #E1E1E1;
 }
-.router-link-exact-active div
+.right_icon
+{
+  width: 0;
+  /* margin-left: 10vw; */
+}
+.router-link-exact-active > div
 {
   color: #36C5F1;
   background: rgba(54, 197, 241, 0.2);
@@ -60,6 +90,12 @@ export default {
   margin-right: 0;
   padding-left: 1.56vw;
   width: 16.66vw;
+}
+.router-link-exact-active img.right_icon
+{
+  width: 0.52vw;
+  vertical-align: middle;
+  text-align: right;
 }
 .recent_scrap
 {
@@ -71,5 +107,23 @@ export default {
   font-size: 1.09vw;
   font-family: NanumBarunGothic;
   columns: #2B2B2B;
+  margin-top: 1.2vw;
+}
+.scrap_icon
+{
+  width: 1.04vw;
+  vertical-align: text-top;
+}
+.card_img
+{
+  width: 100%;
+}
+.card_title
+{
+  font-family: NanumBarunGothic;
+  font-size: 0.94vw;
+  margin-top: 0.79vw;
+  margin-left: 0.78vw;
+  margin-right: 0.78vw;
 }
 </style>
