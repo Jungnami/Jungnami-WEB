07c02f353be08930c768dfcc2ee755d4a5e34a99
kohen.kang <kohen.kang@kakaocorp.com>
2018-07-08 12:43:50 +0900

    로그인 기능 붙임. 일단 rank페이지쪽에 컴포넌트로 달아둠, 3개 리스트 페이징 버그수

--- src/pages/rank/Main.vue
+++ src/pages/rank/Main.vue
@@ -1,26 +1,29 @@
 <template>
-  <!-- <v-layout row wrap md7> -->
-  <v-layout row wrap>
-
-
-    <!-- <v-flex xs1></v-flex> -->
+  <div>
+    <login></login>
+    <!-- <login></login> -->
     <v-layout row wrap>
-      <v-flex xs10 offset-xs1>
-        <rank-box></rank-box>
-      </v-flex>
-      <v-flex xs1></v-flex>
+      <v-layout row wrap>
+        <v-flex xs10 offset-xs1>
+          <rank-box></rank-box>
+        </v-flex>
+        <v-flex xs1></v-flex>
+      </v-layout>
     </v-layout>
+  </div>
 
 
-  </v-layout>
-  <!-- </v-layout> -->
+  <!--s </v-layout> -->
 </template>
 
 <script>
 import RankBox from './components/RankBox'
+// import Modal from '@/pages/legislator/components/SupportModal'
+import Login from '@/components/Login'
+
 export default {
   name: 'Rank',
-  components: { RankBox },
+  components: { RankBox, Login },
   data () {
     return {}
   }
