1718d224e7f567ce35dc8b2ebd97251ab9f06c7f
RyuDongIl <jules010@naver.com>
2018-07-12 16:52:59 +0900

    임시 커밋

--- src/pages/mypage/components/MyVote.vue
+++ src/pages/mypage/components/MyVote.vue
@@ -1,5 +1,9 @@
 <template>
-  
+<v-flex xs10 offset-xs1 class="my_vote">
+  <v-layout row nowrap justify-center>
+    <img src="" alt="change_explain_img" class="change_img">
+  </v-layout>
+</v-flex>  
 </template>
 
 <script>
@@ -8,6 +12,18 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.my_coin
+{
+  border: 1px solid #D3D3D3;
+  border-radius: 4px;
+  box-shadow: 3px 3px 8px rgba(0, 0, 0, 0.16);
+}
+.change_img
+{
+  width: 15.73vw;
+  height: 6vw;
+  margin-top: 2.6vw;
+  margin-bottom: 1.25vw;
+}
 </style>
