9fcea538e0c05cc255ac7073e9126a6471e3a3b4
kohen.kang <kohen.kang@kakaocorp.com>
2018-07-13 23:59:32 +0900

    유튜브 모달 scoped

--- src/pages/contents_detail/components/YoutubeModal.vue
+++ src/pages/contents_detail/components/YoutubeModal.vue
@@ -30,7 +30,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped> 
 .modal_mask
 {
   position: fixed;
@@ -43,7 +43,7 @@ export default {
   display: table;
   transition: opacity .3s ease;
 }
-.modal_wrapper 
+.modal_wrapper
 {
   display: table-cell;
   vertical-align: middle;
