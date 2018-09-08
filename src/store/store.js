c4d428a95ef8ef3eb13e3575983e9faaa3c24c10
RyuDongIl <jules010@naver.com>
2018-09-08 22:28:33 +0900

    쿠키 재생성
    쿠키를 없애지 않겠습니다...

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+import * as Cookies from 'js-cookie'
 import { userGetters, rankGetters, contentsDetailGetters, recommendGetters, partyGetters, legislatorGetters, myPageGetters } from './getters'
 import { loginMutations, rankMutations, contentsDetailMutations, recommendMutations, partyMutations, legislatorMutations, myPageMutations } from './mutations'
 import { loginActions, rankActions, contentsDetailActions, recommendActions, partyActions, legislatorActions, myPageActions } from './actions'
@@ -49,5 +51,20 @@ export const store = new Vuex.Store({
   },
   getters: Object.assign({}, userGetters, rankGetters, contentsDetailGetters, recommendGetters, partyGetters, legislatorGetters, myPageGetters),
   mutations: Object.assign({}, loginMutations, rankMutations, contentsDetailMutations, recommendMutations, partyMutations, legislatorMutations, myPageMutations),
-  actions: Object.assign({}, loginActions, rankActions, contentsDetailActions, recommendActions, partyActions, legislatorActions, myPageActions)
+  actions: Object.assign({}, loginActions, rankActions, contentsDetailActions, recommendActions, partyActions, legislatorActions, myPageActions),
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: key => Cookies.get(key),
+        // Please see https://github.com/js-cookie/js-cookie#json, on how to handle JSON.
+        setItem: (key, value) =>
+          Cookies.set(key, value, { expires: 3, secure: false }), // secure같은 경우는 https일 경우에만 true로 만든다.
+        removeItem: key => Cookies.remove(key)
+      },
+      reducer: state => ({
+        kakaoAccessToken: state.kakaoAccessToken,
+        user_id: state.user_id
+      })
+    })
+  ]
 })
